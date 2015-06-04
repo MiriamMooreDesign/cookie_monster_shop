e7ac3cbb5050a12958963501c8613f09165d741e
MMDesign007 <miriamsusanmoore@gmail.com>
2015-06-04 08:49:44 -0700

    Cookies & Oven Project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cookie_monster_shop
+
+# An object oriented coding exercise done by Miriam Moore and Matthew DeCarlo.
---
+++ cookies_and_ovens.rb
@@ -0,0 +1,122 @@
+Logo = %q"
+  🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪
+🍪    __                       ,_ _                                                  🍪
+🍪   / ()  _   _  |)  o  _    /| | |   _         , _|_  _  ,_      () |)    _      | 🍪
+🍪  |     / \_/ \_|/) | |/     | | |  / \_/|/|  / \_|  |/ /  |     /\ |/\  / \_|/\_| 🍪
+🍪   \___/\_/ \_/ | \/|/|_/    | | |_/\_/  | |_/ \/ |_/|_/   |/   /(_)|  |/\_/ |_/ o 🍪
+🍪                                                                            (|     🍪
+  🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪
+"
+# By M.M.Design and Matthewwho
+require 'csv'
+require 'time'
+
+class Bakery
+  attr_accessor :recipes, :oven
+
+  def initialize
+    @oven = []
+    @recipes = []
+  end
+
+  def get_recipes(filename)
+    CSV.foreach(filename, :headers => true, :header_converters => :symbol) do |recipe_attributes| recipes << Recipe.new(recipe_attributes)
+    end
+  end
+
+  def find_recipe_by_id(id)
+    recipes.each do |a_recipe|
+      return a_recipe if a_recipe.id == id
+    end
+  end
+
+  def display_menu
+    menu = "- - - - - - M E N U - - - - - -\n"
+
+    recipes.each do |a_recipe|
+      menu << "#{a_recipe.id}. #{a_recipe.name}\n"
+    end
+
+    menu << "0. Return to Main Menu\n"
+    menu << "- - - - - - - - - - - - - - - -\n\n\n"
+
+    return menu
+  end
+
+  def prep_batch(id)
+    current_recipe = find_recipe_by_id(id)
+    a_batch = Batch_of_Cookies.new({name:current_recipe.name, ingredients:current_recipe.ingredients})
+  end
+
+  def bake(a_batch)
+    oven << a_batch
+  end
+
+  def remove_cookies!(a_batch)
+    oven.delete(a_batch)
+  end
+
+  def get_batch_status(a_batch)
+    current_time = Time.now
+    if current_time < ( a_batch.start_time + (1 * 60) )
+      return "raw"
+    elsif current_time < ( a_batch.start_time + (5 * 60) )
+      return "gooey"
+    elsif current_time < ( a_batch.start_time + (8 * 60) )
+      return "almost ready"
+    elsif current_time < ( a_batch.start_time + (8 * 60) + 30 )
+      return "done"
+    elsif current_time < ( a_batch.start_time + (9 * 60) )
+      return "a little bit toasty"
+    else
+      return "burnt"
+    end
+  end
+end
+
+class Recipe
+  attr_accessor :id, :name, :ingredients, :bake_time, :baking_temperature
+
+  def initialize(attributes)
+    @id                 = attributes[:id]
+    @name               = attributes[:name]
+    @ingredients        = attributes[:ingredients]
+    @bake_time          = attributes[:bake_time]
+    @baking_temperature = attributes[:baking_temperature]
+  end
+
+  def to_s
+"
+#{self.id}: #{self.name}
+
+Bake Time: #{self.bake_time}
+
+Temperature: #{self.baking_temperature}
+
+Ingredients:
+#{self.ingredients}
+"
+  end
+end
+
+class Batch_of_Cookies
+  attr_accessor :name, :ingredients
+  attr_reader :start_time
+
+  def initialize(attributes)
+    @name         = attributes[:name]
+    @ingredients    = attributes[:ingredients]
+    @start_time     = Time.now
+  end
+
+  def to_s
+"
+A batch of #{self.name} cookies.
+
+Start Time: #{self.start_time}
+
+Ingredients:
+#{self.ingredients}
+"
+  end
+end
